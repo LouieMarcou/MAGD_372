26eafe86d098423bfe1156e90bd66c5a8dd82ff8
Louie Marcou <58005859+LouieMarcou@users.noreply.github.com>
2023-02-21 13:19:43 -0600

    update

--- EventsAndDelegates/Assets/Scripts/EventManager.cs
+++ EventsAndDelegates/Assets/Scripts/EventManager.cs
@@ -13,6 +13,9 @@ public class EventManager : MonoBehaviour
     public delegate void ThrowRightAction();
     public static event ThrowLeftAction OnThrowRight;
 
+    public delegate void IncreaseThrust(float multiplier);
+    public static event IncreaseThrust OnIncreaseThrust;
+
     private void OnGUI()
     {
         if(GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Up"))
@@ -26,6 +29,10 @@ public class EventManager : MonoBehaviour
         if (GUI.Button(new Rect(Screen.width / 2 + 50, 5, 100, 30), "Right"))
         {
             OnThrowRight?.Invoke();
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 50, 100, 30), "Increase Thrust"))
+        {
+            OnIncreaseThrust?.Invoke(1.2f);
         }
     }
 }
